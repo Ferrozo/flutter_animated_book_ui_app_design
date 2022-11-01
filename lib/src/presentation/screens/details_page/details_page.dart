737edb23f771b5f85391473ed7af45f31bde8d49
Ferrozo <ferrozo.c@gmail.com>
2022-11-01 16:17:27 +0100

    Detailed card

--- lib/src/presentation/screens/details_page/details_page.dart
+++ lib/src/presentation/screens/details_page/details_page.dart
@@ -1,3 +1,4 @@
+import 'package:animated_book_ui_app_design/src/domain/repository/cards_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 
@@ -10,30 +11,80 @@ class DetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.end,
+      backgroundColor: Colors.orange,
+      body: SafeArea(
+        child: Stack(
           children: [
-            Container(),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Ionicons.reorder_two_outline,
-                size: 50,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: IconButton(
+                      padding: const EdgeInsets.all(0),
+                      icon: const Icon(
+                        Ionicons.close_outline,
+                        color: Colors.black87,
+                        size: 30,
+                      ),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Ionicons.reorder_two_outline,
+                      size: 50,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
               ),
+            ),
+            Column(
+              children: [
+                const SizedBox(height: 100),
+                Positioned(
+                  child: Image.asset(
+                    cardInfo.cardUrlImage,
+                    width: 230,
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for (int i = 0; i < draggableItems.length; i++)
+                        Container(
+                          margin: const EdgeInsets.only(left: 10),
+                          color: i == 0
+                              ? Colors.white
+                              : const Color.fromARGB(112, 255, 255, 255),
+                          height: 6,
+                          width: 20,
+                        )
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 242.2,
+                  color: Colors.white,
+                  width: MediaQuery.of(context).size.width,
+                  child: const Text('Container'),
+                ),
+              ],
             )
           ],
         ),
-      ),
-      backgroundColor: Colors.orange,
-      body: Center(
-        child: Column(children: [
-          Text(cardInfo.cardTitle.toString()),
-          Image.asset(cardInfo.cardUrlImage),
-        ]),
       ),
     );
   }
